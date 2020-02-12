6b7c9e1fd3662dd839790c176114943ffce7af46
pinkbeamline <pink.group@cec.mpg.de>
2020-02-12 09:51:47 +0100

    logger debug

--- script/local.py
+++ script/local.py
@@ -1,12 +1,15 @@
 ###################################################################################################
 #  Deployment specific global definitions - executed after startup.py
 ###################################################################################################
+from plib.logger_class import MasterLogger
 
 run("plib/bpm_class.py")
 run("plib/pink_extra.py")
 run("plib/pink_class.py")
 run("plib/blade_func.py")
 run("plib/scan_func.py")
+
+mlogger = MasterLogger()
 
 bpm = BPM()
 pink = PINKCLASS()
@@ -17,3 +20,13 @@ slit = SLITS()
 
 print("PShell v13 - New Pink refactored version")
 print("OK")
+
+def on_command_started(info):
+    #mlogger.onstart(info)
+    #print("onstart")
+    pass
+
+def on_command_finished(info):
+    #mlogger.onend(info)
+    #print("onend")
+    pass
--- script/scan1.py
+++ script/scan1.py
@@ -1,2 +1,10 @@
-pink.ge_SEC_EL_continous_exposure_speed(2, 37600, 1100, 3, -3000, 0, 500, passes=2, sample='test1')
-pink.ge_SEC_EL_continous_exposure_speed(2, 37600, 1100, 3, 1000, 4000, 500, passes=2, sample='test1')
+#pink.ge_SEC_EL_continous_exposure_speed(2, 37600, 1100, 3, -3000, 0, 500, passes=2, sample='test1')
+#pink.ge_SEC_EL_continous_exposure_speed(2, 37600, 1100, 3, 1000, 4000, 500, passes=2, sample='test1')
+
+#scan.spot(detector.eiger(), exposure=1, images=3, sample='eiger spot t1')
+#scan.spot(detector.eiger(), exposure=1.3, images=4, sample='eiger spot t2')
+
+print("test1")
+sleep(3)
+print("test2")
+
