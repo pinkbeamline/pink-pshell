ecec44a458ecc16d5d7da84650faba9660b85607
pinkbeamline <pink.group@cec.mpg.de>
2020-09-23 15:17:54 +0200

    update

--- script/Co_Olaf_20200620.py
+++ script/Co_Olaf_20200620.py
@@ -1,6 +1,6 @@
-sample_1='s18 CoFe2O4'
-sample_2='s19 Co0.5Fe2.5O4'
-sample_3='s20 Co1.5Fe1.5O4'
+sample_1='s21 Co(NO3)2 6H2O'
+sample_2='s22 Co2FeO4'
+sample_3='s23 Co2.5Fe0.5O4'
 
 print("########## ENERGYCALIBRATION ##########")
 caput("PINK:SMA01:m10.VAL", 21000.) #X
@@ -11,16 +11,16 @@ scan.spot(detector.eiger(), exposure=5., images=40, sample='Ni foil')
 caput("PINK:SMA01:m9.VAL", -2500.) #Y
 scan.spot(detector.eiger(), exposure=2., images=200, sample='Co foil')
 #Co(OH)2 commercial, Kb1,3 and V2C
-scan.continuous(detector.eiger(), det_exposure=2, sample_exposure=0.5, X0=19500, X1=20000, dX=700, Y0=0, Y1=8100, passes=10, sample='Co(OH)2', linedelay=0)
+scan.continuous(detector.eiger(), det_exposure=2, sample_exposure=0.5, X0=18500, X1=19000, dX=700, Y0=0, Y1=8100, passes=10, sample='Co(OH)2', linedelay=0)
 
 #Measurement
 print("########## MEASUREMENT ##########")
 #1
-scan.continuous(detector.eiger(), det_exposure=5, sample_exposure=0.5, X0=36700, X1=39000, dX=750, Y0=-7500, Y1=8100, passes=6, sample=sample_1, linedelay=0)
+scan.continuous(detector.eiger(), det_exposure=5, sample_exposure=0.5, X0=36700, X1=39000, dX=750, Y0=-7700, Y1=8100, passes=6, sample=sample_1, linedelay=0)
 #2
-scan.continuous(detector.eiger(), det_exposure=5, sample_exposure=0.5, X0=2600, X1=5000, dX=750, Y0=-7500, Y1=8100, passes=12, sample=sample_2, linedelay=0)
+scan.continuous(detector.eiger(), det_exposure=5, sample_exposure=0.5, X0=2600, X1=5000, dX=750, Y0=-7700, Y1=8100, passes=6, sample=sample_2, linedelay=0)
 #3
-scan.continuous(detector.eiger(), det_exposure=5, sample_exposure=0.5, X0=-15200, X1=-12500, dX=750, Y0=-7500, Y1=8100, passes=6, sample='sample_3', linedelay=0)
+scan.continuous(detector.eiger(), det_exposure=5, sample_exposure=0.5, X0=-15200, X1=-12500, dX=750, Y0=-7700, Y1=8100, passes=6, sample=sample_3, linedelay=0)
 
 #Damage scan
 print("########## DAMAGE SCAN ##########")
