8f748bbc9c7ff672a179867ecb812689b0f08028
pinkbeamline <pink.group@cec.mpg.de>
2021-02-25 13:42:02 +0100

    blsetup + continuous ge sec

---
+++ script/config/e2300.py
@@ -0,0 +1,62 @@
+### Energy group ####
+energy      = 2300
+## Undulator gap
+gap         = 
+## M1 positions
+m1tx        = 
+m1ty        = 
+m1rx        = 
+m1ry        = 
+m1rz        = 
+## M1 delta values
+m1deltatx   = 
+m1deltaty   = 
+m1deltatz   = 
+m1deltarx   = 
+m1deltary   = 
+m1deltarz   = 
+## foil filter: 0,5,10,20
+foil        =
+## AU1
+au1centery  =
+au1centerx  =
+au1gapy     =
+au1gapx     =
+## AU2
+au2centery  =
+au2centerx  = 
+au2gapy     =
+au2gapx     =
+## AU3
+au3centery  =
+au3centerx  =
+au3gapy     =
+au3gapx     =
+## M2 positions
+# group options: 1,2 or 3
+m2group     = 1
+m2poix      =
+m2poiy      =
+m2tx        = 
+m2ty        = 
+m2tz        = 
+m2rx        = 
+m2ry        = 
+m2rz        = 
+## M2 delta values
+m2deltatx   = 
+m2deltaty   = 
+m2deltatz   = 
+m2deltarx   = 
+m2deltary   = 
+m2deltarz   = 
+## BPM1 cross
+cross1x     =
+cross1y     =
+## BPM2 cross
+cross2x     =
+cross2y     =
+## BPM3 cross
+cross3x     =
+cross3y     =
+
---
+++ script/config/e4000.py
@@ -0,0 +1,61 @@
+### Energy ####
+energy      = 4000
+## Undulator gap
+gap         = 6.42
+## M1 positions
+m1tx        = 640
+m1ty        = 500
+m1rx        = -1400
+m1ry        = -10500
+m1rz        = 7500
+## M1 delta values
+m1deltatx   = 10
+m1deltaty   = 0
+m1deltarx   = 0.5
+m1deltary   = 10
+m1deltarz   = 10
+## foil filter: 0,5,10,20
+foil        = 20
+## AU1
+au1centery  = 0.6
+au1centerx  = -0.3
+au1gapy     = 0.8
+au1gapx     = 1.6
+## AU2
+au2centery  = 
+au2centerx  = 
+au2gapy     =
+au2gapx     =
+## AU3
+au3centery  = -19.5
+au3centerx  = -2.1
+au3gapy     = 1.2
+au3gapx     = 1.6
+## M2 positions
+# group options: 1,2 or 3
+m2group     = 1
+m2poix      = 24500
+m2poiy      = 2000
+m2tx        = 24500
+m2ty        = 2000
+m2tz        = 0
+m2rx        = -1855
+m2ry        = 500
+m2rz        = 2300
+## M2 delta values
+m2deltatx   = 10
+m2deltaty   = 5
+m2deltatz   = 10
+m2deltarx   = 5
+m2deltary   = 10
+m2deltarz   = 10
+## BPM1 cross
+cross1x     = 633
+cross1y     = 678
+## BPM2 cross
+cross2x     = 670
+cross2y     = 494
+## BPM3 cross
+cross3x     = 558
+cross3y     = 462
+
